6d6d7c24772aa577b463e4e18dcf2d1045e4cf3e
zy_yutao@126.com <zy_yutao@126.com>
2021-04-01 17:16:27 +0800

    1

--- configuration/src/main/resources/db/schema.sql
+++ configuration/src/main/resources/db/schema.sql
@@ -6,3 +6,7 @@ create table vehicle_state(
  description varchar(200) DEFAULT NULL
  );
 
+delete from vehicle_state;
+insert into vehicle_state values('1','1','在用', '车辆正常运行');
+insert into vehicle_state values('2','2','停用', '车辆停止运行');
+insert into vehicle_state values('3','3','待修', '车辆维修');
