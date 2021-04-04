ab72496484c5f32e793cf8ea155ef4e7ac396f2f
zy_yutao@126.com <zy_yutao@126.com>
2021-04-04 11:27:14 +0800

    初始化数据库表

--- configuration/src/main/resources/db/data.sql
+++ configuration/src/main/resources/db/data.sql
@@ -1,4 +1,4 @@
--- delete from vehicle_state;
--- insert into vehicle_state values('1','1','在用', '车辆正常运行');
--- insert into vehicle_state values('2','2','停用', '车辆停止运行');
--- insert into vehicle_state values('3','3','待修', '车辆维修');
+delete from vehicle_state;
+insert into vehicle_state values('1','1','在用', '车辆正常运行');
+insert into vehicle_state values('2','2','停用', '车辆停止运行');
+insert into vehicle_state values('3','3','待修', '车辆维修');
--- configuration/src/main/resources/db/schema.sql
+++ configuration/src/main/resources/db/schema.sql
@@ -5,8 +5,3 @@ create table vehicle_state(
  name varchar(40),
  description varchar(200) DEFAULT NULL
  );
-
-delete from vehicle_state;
-insert into vehicle_state values('1','1','在用', '车辆正常运行');
-insert into vehicle_state values('2','2','停用', '车辆停止运行');
-insert into vehicle_state values('3','3','待修', '车辆维修');
