0535305190c32ff0f4d44977c8c21033f980eadb
zy_yutao@126.com <zy_yutao@126.com>
2021-04-30 15:55:51 +0800

    1

--- configuration/src/main/java/mike/wolf/zygl/IvmsApplication.java
+++ configuration/src/main/java/mike/wolf/zygl/IvmsApplication.java
@@ -2,12 +2,19 @@ package mike.wolf.zygl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class IvmsApplication {
+public class IvmsApplication  extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(IvmsApplication.class, args);
   }
 
+  @Override//为了打包springboot项目
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return super.configure(builder);
+//    return builder.sources(this.getClass());
+  }
 }
