38a0be490809b80e1158526d20c41e9f9496353d
zy_yutao@126.com <zy_yutao@126.com>
2021-05-03 23:20:46 +0800

    1

--- adapters/web/src/main/java/mike/wolf/zygl/adapter/web/model/FormVehicleStateDTO.java
+++ adapters/web/src/main/java/mike/wolf/zygl/adapter/web/model/FormVehicleStateDTO.java
@@ -2,14 +2,7 @@ package mike.wolf.zygl.adapter.web.model;
 
 import lombok.*;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-
-//@Value
-//@Builder
-
+@Data
 public class FormVehicleStateDTO {
     private String id;
     private String name;
