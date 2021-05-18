11eeeb487c0bbbd27886ec347448fa2b6f5a6ee6
zy_yutao@126.com <zy_yutao@126.com>
2021-05-18 11:43:32 +0800

    1

--- configuration/src/main/resources/db/data.sql
+++ configuration/src/main/resources/db/data.sql
@@ -8,6 +8,18 @@ insert into vehicle_type values('1','锅炉车', '锅炉车主要是用来');
 insert into vehicle_type values('2','水泥车', '水泥车是打压用的');
 insert into vehicle_type values('3','污水罐车', '水');
 
+delete from vehicle_parameter;
+insert into vehicle_parameter(id, rylx_tx) values('1','汽油');
+insert into vehicle_parameter (id, rylx_tx) values('2','柴油');
+
+delete from vehicle_special;
+insert into vehicle_special(id, fdjxh_tx) values('1','发动机型号1');
+insert into vehicle_special (id, fdjxh_tx) values('2','发动机型号2');
+
+delete from vehicle_structure;
+insert into vehicle_structure(id, fxpwz_tx) values('1','方向盘位置-1');
+insert into vehicle_structure (id, fxpwz_tx) values('2','方向盘位置-12');
+
 delete from vehicle;
-insert into vehicle values('1','车辆基础信息', 'E123','Z123', '1','1','在用锅炉车');
-insert into vehicle values('2','车辆基础信息222', 'E456','Z456', '2','2','停用水泥车');
+insert into vehicle(id, name_tx, ggxh_tx, vehicle_state_id, vehicle_type_id, vehicle_structure_id, vehicle_parametere_id, vehicle_special_id) values('1','车辆基础信息', 'E123','1', '1','1','1','1');
+insert into vehicle(id, name_tx, ggxh_tx, vehicle_state_id, vehicle_type_id, vehicle_structure_id, vehicle_parametere_id, vehicle_special_id) values('1','车辆基础信息2', 'E123','2', '2','2','2','2');
--- configuration/src/main/resources/db/schema.sql
+++ configuration/src/main/resources/db/schema.sql
@@ -1,27 +1,110 @@
- DROP TABLE IF EXISTS `vehicle_state`;
-create table vehicle_state(
- id  varchar(40) PRIMARY KEY ,
- name varchar(40),
- description varchar(200) DEFAULT NULL
- );
+DROP TABLE IF EXISTS `vehicle_state`;
+create table vehicle_state
+(
+    id          varchar(40) PRIMARY KEY,
+    name        varchar(40),
+    description varchar(200) DEFAULT NULL
+);
 
- DROP TABLE IF EXISTS `vehicle_type`;
-create table vehicle_type(
- id  varchar(40) PRIMARY KEY ,
- name varchar(40),
- description varchar(200) DEFAULT NULL
- );
+DROP TABLE IF EXISTS `vehicle_type`;
+create table vehicle_type
+(
+    id          varchar(40) PRIMARY KEY,
+    name        varchar(40),
+    description varchar(200) DEFAULT NULL
+);
 
- DROP TABLE IF EXISTS `vehicle`;
-create table vehicle(
-                         id  varchar(40) PRIMARY KEY ,
-                         name varchar(40),
-                         pz varchar(40),
-                         nbpz varchar(40),
-                         vehicle_type_id varchar(40),
-                         vehicle_state_id varchar(40),
-                         description varchar(200) DEFAULT NULL,
-                         constraint type_vehicle_fk foreign key (vehicle_type_id) REFERENCES vehicle_type(id),
-                         constraint state_vehicle_fk foreign key (vehicle_state_id) REFERENCES vehicle_state(id)
+DROP TABLE IF EXISTS `vehicle_structure`;
+create table vehicle_structure
+(
+    id             varchar(40) PRIMARY KEY,
+    cc_nb          number(4),
+    ck_nb          number(4),
+    cg_nb          number(4),
+    zj_nb          number(4),
+    qlj_nb         number(4),
+    hlj_nb         number(4),
+    qdxs_tx        varchar(40),
+    fxpwz_tx       varchar(40),
+    bsqxs_tx       varchar(40),
+    description_tx varchar(500)
 
- );
+);
+
+DROP TABLE IF EXISTS `vehicle_parameter`;
+create table vehicle_parameter
+(
+    id             varchar(40) PRIMARY KEY,
+    zczbzl_nb      number(4),
+    zdzzzl_nb      number(4),
+
+    rylx_tx        varchar(40),
+
+    pjyh_nb        number(4),
+    edgl_nb        number(4),
+    zdnj_nb        number(4),
+    zxzwbj_nb      number(4),
+    zgcs_nb        number(4),
+    description_tx varchar(500)
+
+);
+
+DROP TABLE IF EXISTS `vehicle_special`;
+create table vehicle_special
+(
+    id             varchar(40) PRIMARY KEY,
+    zdqzl_nb       number(4),
+    gjbj_nb        number(4),
+    zb_nb          number(4),
+    zbc_nb         number(4),
+    fbc_nb         number(4),
+    fdjxh_tx       varchar(40),
+    edgl_nb        number(4),
+    zdnj_nb        number(4),
+    zgzs_nb        number(4),
+    rylx_tx        varchar(40),
+    pjyh_nb        number(4),
+    glxh_tx        varchar(40),
+    ysjxh_tx       varchar(40),
+    bxh_tx         varchar(40),
+    csyq_tx        varchar(40),
+    dr_nbb         number(4),
+    bsqxs_tx       varchar(40),
+
+    description_tx varchar(500)
+
+
+);
+
+DROP TABLE IF EXISTS `vehicle`;
+create table vehicle
+(
+    id                    varchar(40) PRIMARY KEY,
+    name_tx               varchar(40),
+    ggxh_tx               varchar(40),
+    pz_tx                 varchar(40),
+    nbpz_tx               varchar(40),
+    sccj_tx               varchar(40),
+
+    ccrq_dt               date,
+    tcrq_dt               date,
+    yz_nb                 number(4),
+    csys_tx               varchar(40),
+    fdjxh_tx              varchar(40),
+    fdjbh_tx              varchar(40),
+    dpxh_tx               varchar(40),
+    dpbh_tx               varchar(40),
+    description_tx        varchar(500),
+    vehicle_state_id      varchar(40),
+    vehicle_type_id       varchar(40),
+    vehicle_structure_id  varchar(40),
+    vehicle_parametere_id varchar(40),
+    vehicle_special_id    varchar(40),
+    constraint type_vehicle_fk foreign key (vehicle_type_id) REFERENCES vehicle_type (id),
+    constraint state_vehicle_fk foreign key (vehicle_state_id) REFERENCES vehicle_state (id),
+    constraint structure_vehicle_fk foreign key (vehicle_structure_id) REFERENCES vehicle_structure (id),
+    constraint parametere_vehicle_fk foreign key (vehicle_parametere_id) REFERENCES vehicle_parameter (id),
+    constraint type_special_fk foreign key (vehicle_special_id) REFERENCES vehicle_special (id)
+
+);
+
